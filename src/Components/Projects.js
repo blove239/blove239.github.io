331355499be3bd5592e75eb95dbaa62afb9a0dfe
Brandon <blove239@gmail.com>
2021-02-01 15:31:43 -0500

    Added ImageRepo Project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Thumbnail from './Thumbnail';
 import TypingTestScreenCap from '../images/typing test screenshot.png';
+import ImageRepoScreenCap from '../images/imagereposcreenshot.png';
 
 const Projects = () => (
   <div className='container my-5 mx-auto'>
@@ -8,10 +9,16 @@ const Projects = () => (
       <div className='col-lg-11'>
         <h1>Projects</h1>
         <Thumbnail
-          link='http://typingtest.brandonlove.ca/'
+          link='https://typingtest.brandonlove.ca/'
           image={TypingTestScreenCap}
           title='Typing Test'
           description='A full stack typing test with leaderboard functionality. Uses React for the front end. NeDB, Express.js and Node.js for the backend. Hosted on an Amazon EC2 instance.'
+        />
+        <Thumbnail
+          link='https://imagerepo.brandonlove.ca/'
+          image={ImageRepoScreenCap}
+          title='Image Repo'
+          description='A simple image hosting repository. Uses Passport.js for user auth, SQLite for storage, served with Express.js. Hosted on an Amazon EC2 instance.'
         />
       </div>
     </div>
--- src/Components/Thumbnail.js
+++ src/Components/Thumbnail.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Thumbnail = ({ image, link, title, description }) => (
-  <div className='container text-center'>
+  <div className='container text-center my-5'>
     <span className='project-title'>{title}</span>
     {' – '}
     <span className='project-description'>{description}</span>
     <div className='row justify-content-center'>
       <div className='text-center col-lg-5'>
         <a href={link}>
-          <img className='img-fluid project-image' src={image} alt='Typing Test made with React' />
+          <img className='img-fluid project-image border border-secondary' src={image} alt='Project Screenshot' />
         </a>
       </div>
     </div>
